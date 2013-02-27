cfb7d910a14f8ce6d29398fb6c9ff8b37d9827fe
Bikram Chatterjee <razorpeak@gmail.com>
2013-02-27 15:28:51 +0100

    restructured into dll format

---
+++ console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Erlang.NET;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OtpErlangAtom atom = new OtpErlangAtom("test");
+            OtpOutputStream otps = new OtpOutputStream(atom);
+            byte[] buf = otps.GetBuffer();
+            Console.WriteLine("Executed!" + buf);
+        }
+    }
+}
